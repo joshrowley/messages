ef6cd6054c70a49d248bacdf3547664895143238
Josh Rowley <joshua.f.rowley@gmail.com>
2012-10-15 16:07:52 -0400

    Add image embedding

--- app.rb
+++ app.rb
@@ -21,15 +21,34 @@ post '/' do
   # TODO: Read the message contents, save to the database
   message_contents = request.POST['message']
   words = message_contents.split
+  # Creates links around any input that ends in .com
   links = words.map do |word|
   	word if word.end_with?(".com")
   end
   links.compact!
+
   links.each do |link|
   	if link.start_with?("http://")
   		message_contents[link] = "<a href='#{link}'>#{link}</a>"
   	else
   		message_contents[link] = "<a href='http://#{link}'>#{link}</a>"
+  	end
+  end
+
+  # Creates img tags around any image
+
+  images = words.map do |word|
+  	if word.end_with?(".jpg") || word.end_with(".gif") || word.end_with(".png")
+  		word
+  	end
+  end
+  images.compact!
+
+  images.each do |image|
+  	if image.start_with?("http://")
+  		message_contents[image] = "<img src='http://#{image}'>"
+  	else
+  		message_contents[image] = "<img src='#{image}'>"
   	end
   end
 
